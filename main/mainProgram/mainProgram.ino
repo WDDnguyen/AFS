36ed8ddaf322e97ae37fc8a1e4aafa85c955095a
steventir <steven.tir@mail.mcgill.ca>
2017-09-30 15:38:44 -0400

    Applies avg filter to temp & humidity sensor

--- main/mainProgram/mainProgram.ino
+++ main/mainProgram/mainProgram.ino
@@ -1,17 +1,19 @@
 //Libraries
 #include "dht.h"
+#include "MovingAverageFilter.h"
 dht DHT;
+MovingAverageFilter movingAverageFilter(15);
 
-//Constants
+//Constants, on schematic, pin - 1
 #define DHT22_PIN       2   // Temperature and Humidity Sensor
 #define LED_ROW_1       3   // First row of each LED Strip
 #define LED_ROW_2       4   // Second row of each LED Strip
 #define PIPE_SERVO_1    5    
 #define PIPE_SERVO_2    6     
 #define RELAY1_IN_1     7   // Mix tank pump        
-#define RELAY1_IN_2     8   // Heater
-#define RELAY2_IN_1     9   // Fan
-#define RELAY2_IN_2     10  // Dripping tank pump
+#define RELAY1_IN_2     8   // Fan
+#define RELAY2_IN_1     9   // Dripping tank pump
+#define RELAY2_IN_2     10  // Heater
 #define AIR_PUMP        11  
 #define SOLUTION_PUMP   12
 #define WATER_PUMP      13 
@@ -22,6 +24,8 @@ float temp;         //Stores temperature value
 int chk;            //Read sensor value
 String humidity;    // display humidity
 String temperature; // display temperature
+int hum_filter[10];
+int temp_filter[10];
 
 void setup()
 {
@@ -42,13 +46,17 @@ void setup()
 void pollTempSensor(){
     //Temperature and Humidity Sampling
     chk = DHT.read22(DHT22_PIN);
-    hum = DHT.humidity;
-    temp= DHT.temperature;
+    hum = movingAverageFilter.process(DHT.humidity);
+    temp= movingAverageFilter.process(DHT.temperature);
+
     //Print temp and humidity values to serial monitor
     humidity = String(hum);
     temperature = String(temp);
     Serial.print("Humidity: " + humidity + " %, Temperature : " + temperature + " Celsius \n");
-    //delay(2000); //Delay 2 sec.
+    Serial.print("Without Filter => Humidity: " + String(DHT.humidity) + " %, Temperature : " + String(DHT.temperature) + " Celsius \n");
+
+    
+    delay(2000); //Delays 2 sec.
 }
 
 void setLEDRows(){
@@ -111,18 +119,18 @@ void disableRelays(){
 void loop()
 {
     pollTempSensor();
-    setLEDRows();
-    setWaterPump();
-    setSolutionPump();
-    setAirPump();
-    setPipeMotors();
-    setRelays();
-    delay(1000);
-    disablePipeMotors();
-    disableLEDRows();
-    disableWaterPump();
-    disableSolutionPump();
-    disableAirPump();
-    disableRelays();
-    delay(1000);
+//    setLEDRows();
+//    setWaterPump();
+//    setSolutionPump();
+//    setAirPump();
+//    setPipeMotors();
+//    setRelays();
+//    delay(1000);
+//    disablePipeMotors();
+//    disableLEDRows();
+//    disableWaterPump();
+//    disableSolutionPump();
+//    disableAirPump();
+//    disableRelays();
+//    delay(1000);
 }
