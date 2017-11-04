bec9a4053c6a4e210af237a35829c251d46c48de
steventir <steven.tir@mail.mcgill.ca>
2017-11-04 17:42:28 -0400

    adds EC Sensor code to main

--- main/mainProgram/mainProgram.ino
+++ main/mainProgram/mainProgram.ino
@@ -1,12 +1,18 @@
 //Libraries
 #include "dht.h"
 #include "MovingAverageFilter.h"
+#include "OneWire.h"
+
 dht DHT;
-MovingAverageFilter movingAverageFilter(15);
+MovingAverageFilter movingAverageFilter(15); //might be necessary to create mult objects for mul
+
 
 //Constants, on schematic, pin - 1
-#define PH_SENSOR       0
-#define DHT22_PIN       2   // Temperature and Humidity Sensor
+#define PH_SENSOR       0   // pH Sensor (POLL)
+#define EC_SENSOR       1   // EC Sensor (POLL)
+#define EC_TEMPSENSOR   2   // Liquid TEMP Sensor (POLL)
+#define DHT22_PIN       2   // Temperature and Humidity Sensor (POLL)
+
 #define LED_ROW_1       3   // First row of each LED Strip
 #define LED_ROW_2       4   // Second row of each LED Strip
 #define PIPE_SERVO_1    5    
@@ -19,17 +25,23 @@ MovingAverageFilter movingAverageFilter(15);
 #define SOLUTION_PUMP   12
 #define WATER_PUMP      13
 
+//For the EC Sensor
+#define StartConvert 0
+#define ReadTemperature 1
 
 //Variables
 float hum;          //Stores humidity value
 float temp;         //Stores temperature value
 float ph_offset = 0.00;
 float ph;
+float ECcurrent;
 int chk;            //Read sensor value
 String humidity;    // display humidity
 String temperature; // display temperature
 int hum_filter[10];
 int temp_filter[10];
+
+OneWire ds(EC_TEMPSENSOR);
 
 void setup()
 {
@@ -45,6 +57,7 @@ void setup()
     pinMode(AIR_PUMP, OUTPUT);
     pinMode(SOLUTION_PUMP, OUTPUT);
     pinMode(WATER_PUMP, OUTPUT);
+    TempProcess(StartConvert);
 }
 
 void pollTempSensor(){
@@ -69,6 +82,75 @@ void pollPHSensor() {
     ph = movingAverageFilter.process(rawPH);
     Serial.println("PH avg value:" + String(ph));
     delay(2000);
+}
+
+void pollECSensor() {
+    float avgAnalogValue = movingAverageFilter.process(analogRead(EC_SENSOR));
+    //Serial.println(String(avgAnalogValue));
+    float temperature = TempProcess(ReadTemperature);  // read the current temperature from the  DS18B20
+    TempProcess(StartConvert);                   //after the reading,start the convert for next reading
+Serial.println(String(temperature));
+    
+    float avgVoltage = avgAnalogValue * (float) 5000/1024;
+    float TempCoefficient=1.0+0.0185*(temperature-25.0);    //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.0185*(fTP-25.0));
+    float CoefficientVolatge=(float)avgVoltage/TempCoefficient;   
+    Serial.println(String(CoefficientVolatge));
+    if(CoefficientVolatge<150)Serial.println("No solution!");   //25^C 1413us/cm<-->about 216mv  if the voltage(compensate)<150,that is <1ms/cm,out of the range
+    else if(CoefficientVolatge>3300)Serial.println("Out of the range!");  //>20ms/cm,out of the range
+    else
+    { 
+      if(CoefficientVolatge<=448)ECcurrent=6.84*CoefficientVolatge-64.32;   //1ms/cm<EC<=3ms/cm
+      else if(CoefficientVolatge<=1457)ECcurrent=6.98*CoefficientVolatge-127;  //3ms/cm<EC<=10ms/cm
+      else ECcurrent=5.3*CoefficientVolatge+2278;                           //10ms/cm<EC<20ms/cm
+      ECcurrent/=1000;    //convert us/cm to ms/cm
+      Serial.print(ECcurrent,2);  //two decimal
+      Serial.println("ms/cm");
+    }
+
+    delay(2000);
+}
+
+/*
+ch=0,let the DS18B20 start the convert;ch=1,MCU read the current temperature from the DS18B20.
+*/
+float TempProcess(bool ch)
+{
+  //returns the temperature from one DS18B20 in DEG Celsius
+  static byte data[12];
+  static byte addr[8];
+  static float TemperatureSum;
+  if(!ch){
+          if ( !ds.search(addr)) {
+              Serial.println("no more sensors on chain, reset search!");
+              ds.reset_search();
+              return 0;
+          }      
+          if ( OneWire::crc8( addr, 7) != addr[7]) {
+              Serial.println("CRC is not valid!");
+              return 0;
+          }        
+          if ( addr[0] != 0x10 && addr[0] != 0x28) {
+              Serial.print("Device is not recognized!");
+              return 0;
+          }      
+          ds.reset();
+          ds.select(addr);
+          ds.write(0x44,1); // start conversion, with parasite power on at the end
+  }
+  else{  
+          byte present = ds.reset();
+          ds.select(addr);    
+          ds.write(0xBE); // Read Scratchpad            
+          for (int i = 0; i < 9; i++) { // we need 9 bytes
+            data[i] = ds.read();
+          }         
+          ds.reset_search();           
+          byte MSB = data[1];
+          byte LSB = data[0];        
+          float tempRead = ((MSB << 8) | LSB); //using two's compliment
+          TemperatureSum = tempRead / 16;
+    }
+          return TemperatureSum;  
 }
 
 void setLEDRows(){
@@ -131,7 +213,8 @@ void disableRelays(){
 void loop()
 {
 //    pollTempSensor();
-      pollPHSensor();
+//      pollPHSensor();
+      pollECSensor();
 //    setLEDRows();
 //    setWaterPump();
 //    setSolutionPump();
